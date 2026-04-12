c7ac765fa821b50c66c940df271e1de2215e7425
Abhishek97-co <abhishekgupta908453908453@gmail.com>
2026-04-13 01:08:12 +0530

    final fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,9 +15,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: "https://chatapplication-wine.vercel.app",
+  credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
