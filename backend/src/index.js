59d97801e71f4e989f8b3937400e9ad81f919a42
Abhishek97-co <abhishekgupta908453908453@gmail.com>
2026-04-12 22:45:10 +0530

    Initial commit with gitignore

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js"; 
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import path from "path";
+dotenv.config();
+import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
+
+const PORT = process.env.PORT ;
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages",messageRoutes);
+server.listen(PORT,()=>{
+    console.log("server is running");
+    connectDB();
+});
